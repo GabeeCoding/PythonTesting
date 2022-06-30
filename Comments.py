f0c63fd8931dc7425336b8bfa24926b4ff50271b
Gabes <gabnology+github@gmail.com>
2022-06-30 16:24:16 +0000

    Comments

---
+++ Comments.py
@@ -0,0 +1,11 @@
+# To add a comment
+# Use a hashtag
+# A hashtag can be used to prevent a line of code from running without removing it
+#print("Hello World")
+
+"""
+Or you can use a multiline string
+Python ignores strings not assigned to a variable
+Which allows for multiline comments
+
+"""
