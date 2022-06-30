6cc7ce7f01551aa2bebfb2f10a2a05d2b7ca4f09
Gabes <gabnology+github@gmail.com>
2022-06-30 16:24:44 +0000

    Add if statements

---
+++ IfStatements.py
@@ -0,0 +1,10 @@
+bananas = 3
+apples = 4
+pears = 1
+
+if (pears > apples):
+    print("There are more pears than apples")
+elif pears == apples: # elif means elseif
+    print("The amount of pears and apples are equal")
+else:
+    print("There are more apples than pears")
