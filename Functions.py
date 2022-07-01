5dfcda284df4570ca8c192f56651a4b74774e349
Gabes <gabnology+github@gmail.com>
2022-07-01 15:19:27 +0000

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,22 @@
+v = 1
+y = 0
+def func(): # basic function which prints when called
+    print("Works")
+
+func() #call the function
+
+def vartest():
+    # global and local variables
+    # if you set a variable in a function with the same name as a global variable
+    # it is overidden in the fuction
+    v = 2
+    print(v) # 2
+    # if you want to set a global variable in a function
+    # use the global keyword
+    global y
+    y = 1
+    print(y) # 1
+
+vartest()
+print(v) # v is 1 and not 2 because it was set locally in the function
+print(y) # y is still 1 because of the global keyword
